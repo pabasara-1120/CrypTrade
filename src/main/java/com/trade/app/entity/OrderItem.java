dda16d542df45c9846c4742e3f0dd1bf8d45c900
Pabasara Jayasekera <108865623+pabasara-1120@users.noreply.github.com>
2024-12-25 17:01:32 +0530

    feat(model): implement OrderItem entity class, along with the repository and service

---
+++ src/main/java/com/trade/app/entity/OrderItem.java
@@ -0,0 +1,35 @@
+package com.trade.app.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private double quantity;
+
+    @ManyToOne
+    private Coin coin;
+
+    private double buyingPrice;
+
+    private double sellingPrice;
+
+    @JsonIgnore
+    @OneToOne
+    private Order order;
+
+
+}
---
+++ src/main/java/com/trade/app/service/OrderItemService.java
@@ -0,0 +1,5 @@
+package com.trade.app.service;
+
+public interface OrderItemService {
+
+}
