abdef12494e6f00a680351b693e25db6dfedb40f
Pabasara Jayasekera <jayasekeramp@gmail.com>
2025-01-09 20:27:34 +0530

    fix(auth): update watchlist entity

--- src/main/java/com/trade/app/entity/WatchList.java
+++ src/main/java/com/trade/app/entity/WatchList.java
@@ -1,8 +1,10 @@
 package com.trade.app.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +12,14 @@ import java.util.List;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WatchList {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     private User user;
 
     @ManyToMany
